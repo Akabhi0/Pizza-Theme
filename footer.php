64f31dcb0803d7e8863a0acd578e1da3e6fb30cc
abhishek abhi Abhi <abhishekabhiabhi0@gmail.com>
2017-02-07 23:52:56 +0530

    this is the add part of footer.php

--- footer.php
+++ footer.php
@@ -1,4 +1,35 @@
- <div id="footer"> this is the part of footer
+ <div id="footer"> 
+    <ul>
+	<li id="footer1">
+          <center><ul id="widget">
+	              <li id="footer_news">
+				    <div><a href="" class="widget-title"><img src="http://localhost/wp-content/uploads/2017/02/news.png">Recent News</a></div>
+                    <?php  if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('news widget') ): ?>
+	                <?php endif; ?>				  
+			      </li>
+				  
+				  <li>
+                  <?php  if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('work widget') ): ?>
+	              <?php endif; ?>		  
+			      </li>
+				  
+				  <li>
+				  <?php  if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('offer widget') ): ?>
+	              <?php endif; ?>
+			      </li>
+				  
+				  <li>
+	                <?php  if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('social widget') ): ?>
+	                <?php endif; ?>
+			      </li>	  
+	              </ul></center>
+	</li>
+	<li id="footer2">
+	   <div id="red"> </div>
+	</li>
+	<li id="footer3"> footer 3 </li>
+ 
+    </ul>
     <?php wp_footer(); ?>
    </div>
 </body>
